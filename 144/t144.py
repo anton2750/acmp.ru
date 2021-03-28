14cf7e5129d82015f058a0830438c044611900ec
Anton <anton2750@mail.ru>
2021-03-28 13:12:18 +0300

    144 one line solution

--- 144/t144.py
+++ 144/t144.py
@@ -1,3 +1,4 @@
-f = open("input.txt", "r")
-g = open("output.txt", "w")
-g.write(str(int(f.readline()) * int(f.readline())))
+# f = open("input.txt", "r")
+# g = open("output.txt", "w")
+# g.write(str(int(f.readline()) * int(f.readline())))
+print(int(input()) * int(input()))
