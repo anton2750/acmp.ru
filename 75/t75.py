f3d073dd56933514c729dbd76ae9c6f275961c36
Anton <anton2750@mail.ru>
2021-03-29 23:14:07 +0300

    holly sh!t

--- 75/t75.py
+++ 75/t75.py
@@ -2,20 +2,31 @@ def main():
     with open("input.txt", "r") as f:
         n = int(f.readline())
 
-    # 1-9 LOOP does not work
-    def mult(lvl):
-        r = 0
-        for i in range(1, 10):
-            if lvl > 1:
-                r += i * mult(lvl - 1)
-            else:
-                r += i
-        return r
+    # def counter(lvl):
+    #     r = 0
+    #     for i in range(1, 10):
+    #         if lvl > 1:
+    #             r += counter(lvl - 1)
+    #         else:
+    #             r += 1
+    #     return r
+    #
+    # count = counter(n)
 
-    r = mult(n)
+    # 1-9 LOOP does not work
+    # def mult(lvl):
+    #     r = 0
+    #     for i in range(1, 10):
+    #         if lvl > 1:
+    #             r += i * mult(lvl - 1)
+    #         else:
+    #             r += i
+    #     return r
+    #
+    # result = mult(n)
 
     # BRUTE FORCE does not work
-    # r = 0
+    # result = 0
     # for i in range(10 ** (n - 1), 10 ** n):
     #     t = 1
     #     c = i
@@ -24,12 +35,14 @@ def main():
     #         if c % 10 == 0:
     #             break
     #         c = c // 10
-    #     r += t
+    #     result += t
 
-    print(r)
+    # print('n =', n)
+    # print('c =', count)
+    # print('r =', result)
 
     with open("output.txt", "w") as g:
-        g.write(str(r))
+        g.write(str((5 ** n) * (9 ** n)))
 
 
 if __name__ == "__main__":
