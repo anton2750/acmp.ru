363e960eb6190cbd3e19d2988cff99562ecb64c2
Anton Erogov <anton2750@mail.ru>
2022-01-30 19:47:03 +0300

    any function test

--- 754/script.py
+++ 754/script.py
@@ -2,9 +2,9 @@ with open("input.txt", "r") as f:
     with open("output.txt", "w") as o:
         [m1, m2, m3] = list(map(int, f.readline().split()))
         # 94, 727
-        if not all(x >= 94 for x in [m1, m2, m3]):
+        if any(x < 94 for x in [m1, m2, m3]):
             o.write('Error')
-        elif not all(x <= 727 for x in [m1, m2, m3]):
+        elif any(x > 727 for x in [m1, m2, m3]):
             o.write('Error')
         else:
             o.write(f"{max([m1, m2, m3])}")
