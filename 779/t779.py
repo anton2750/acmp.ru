294a38dda8ade6411e90aca998ae6054aa5f0e85
Anton Erogov <anton2750@mail.ru>
2022-02-01 12:44:51 +0300

    what am i doing wrong

--- 779/t779.py
+++ 779/t779.py
@@ -1,25 +1,6 @@
-def int_r(num):
-    num = int(num + (0.5 if num > 0 else -0.5))
-    return num
+from math import ceil, floor
 
-
-def main():
-    r = 0
-    with open("input.txt", "r") as f:
-        n = int(f.readline())
-
-        for i in range(n):
-            a = ""
-            while True:
-                l = f.read(1)
-                if l == ' ' or l == '':
-                    break
-                a += l
-            r += int(a) / n
-
-    with open("output.txt", "w") as g:
-        g.write(str(int_r(r)))
-
-
-if __name__ == "__main__":
-    main()
+n = int(input())
+a = tuple(map(int, input().split()))
+r = sum(a) / n
+print(ceil(r) if r >= 0 else floor(r))
