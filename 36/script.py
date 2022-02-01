c508091627b1cb72503f05f4524c819fbbe9a98f
Anton Erogov <anton2750@mail.ru>
2022-02-01 12:03:22 +0300

    some error, cant understand

--- 36/script.py
+++ 36/script.py
@@ -1,20 +1,22 @@
 from math import sqrt
 from math import ceil
 
-
-def is_simple(a):
-    for i in range(2, ceil(sqrt(a)) + 1):
-        if a % i == 0:
-            return False
-    return True
+# def is_simple(a):
+#     for j in range(2, ceil(sqrt(a)) + 1):
+#         if a % j == 0:
+#             return False
+#     return True
 
 
 with open("input.txt", "r") as f:
     with open("output.txt", "w") as o:
         a = int(f.readline())
         c = 0
-        for i in range(a + 1, 2 * a):
-            if is_simple(i):
-                c += 1
+        for t in range(a + 1, 2 * a):
+            c += 1
+            for j in range(2, ceil(sqrt(a))):
+                if t % j == 0:
+                    c -= 1
+                    break
 
         o.write(f'{c}')
