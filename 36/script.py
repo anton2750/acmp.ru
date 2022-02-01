cfdcc5072b925f6279324aabca813690c65811f4
Anton Erogov <anton2750@mail.ru>
2022-02-01 12:13:24 +0300

    new version without IO functions

--- 36/script.py
+++ 36/script.py
@@ -1,15 +1,13 @@
 from math import sqrt
 from math import ceil
 
-with open("input.txt", "r") as f:
-    with open("output.txt", "w") as o:
-        a = int(f.readline())
-        c = 0
-        for t in range(a + 1, 2 * a):
-            c += 1
-            for j in range(2, ceil(sqrt(t)) + 1):
-                if t % j == 0:
-                    c -= 1
-                    break
+a = int(input())
+c = 0
+for t in range(a + 1, 2 * a):
+    c += 1
+    for j in range(2, ceil(sqrt(t)) + 1):
+        if t % j == 0:
+            c -= 1
+            break
 
-        o.write(f'{c}')
+print(c)
