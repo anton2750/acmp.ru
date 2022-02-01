068408746656a0e1c103aa9e7b2bcec40d0d54fc
Anton Erogov <anton2750@mail.ru>
2022-02-01 12:11:16 +0300

    new version without a function

--- 36/script.py
+++ 36/script.py
@@ -1,20 +1,13 @@
 from math import sqrt
 from math import ceil
 
-# def is_simple(a):
-#     for j in range(2, ceil(sqrt(a)) + 1):
-#         if a % j == 0:
-#             return False
-#     return True
-
-
 with open("input.txt", "r") as f:
     with open("output.txt", "w") as o:
         a = int(f.readline())
         c = 0
         for t in range(a + 1, 2 * a):
             c += 1
-            for j in range(2, ceil(sqrt(a))):
+            for j in range(2, ceil(sqrt(t)) + 1):
                 if t % j == 0:
                     c -= 1
                     break
