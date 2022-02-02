31eb478761d7173c741ef427bf8ad3e1a2e4d4da
Anton Erogov <anton2750@mail.ru>
2022-02-02 12:08:23 +0300

    preparation done

--- 561/script.py
+++ 561/script.py
@@ -2,19 +2,31 @@ import math
 
 # logs pre calcs:
 d = {(k, v): math.log(k, v) for k in range(2, 100) for v in range(2, 100)}
+t = {k: 1 / k for k in range(1, 100)}
 
 
 class Tower:
-    def __init__(self, data):
+    # magic:
+    def formula(self):
+        return str(self.base) + '**' + '**'.join(list(map(str, self.powers)))
+
+    def __init__(self, i, data):
+        self.i = i
         self.exp_count = data[0]
         self.base = data[1]
-        self.powers = data[2:] + [1] * (10 - len(data[2:]))
+        # self.powers = data[2:] + [1] * (10 - len(data[2:])) # if needed
+        self.powers = data[2:]
+
+        self.value = eval(self.formula())
 
     def __str__(self):
-        return str(self.base) + ' ** ' + '**'.join(list(map(str, self.powers)))
+        return str(self.i)
 
     def __repr__(self):
-        return str(self.base) + ' ** ' + '**'.join(list(map(str, self.powers)))
+        return str(self.i)
+
+    def __lt__(self, other):
+        return self.value < other.value
 
     # def value(self):
     #     return 1
@@ -22,11 +34,13 @@ class Tower:
 
 with open("input.txt", "r") as f:
     with open("output.txt", "w") as o:
-        # heavy load:
-        total = int(f.readline())
-        towers = {k: Tower(list(map(int, f.readline().split()))) for k in range(1, total + 1)}
+        # heavy metal:
 
-        print(towers)
+        towers = [Tower(i, list(map(int, f.readline().split()))) for i in range(1, int(f.readline()) + 1)]
+        o.write(' '.join([str(x) for x in sorted(towers)]))
+
+        # print(t)
+        # print(d)
 
         # example how to use precalcs:
         # print(d[(2, 2)])
