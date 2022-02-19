765b7d753ad2f731afb4c54b5f4dba39b37826cc
Anton Erogov <anton2750@mail.ru>
2022-02-19 22:58:55 +0300

    678 short

--- 678/script.py
+++ 678/script.py
@@ -1,20 +1,11 @@
 e = {1: 1, 2: 0, 3: 0}
 
+for i in input():
+    if i == 'A':
+        e[1], e[2] = e[2], e[1]
+    elif i == 'B':
+        e[3], e[2] = e[2], e[3]
+    else:
+        e[1], e[3] = e[3], e[1]
 
-def a():
-    e[1], e[2] = e[2], e[1]
-
-
-def b():
-    e[3], e[2] = e[2], e[3]
-
-
-def c():
-    e[1], e[3] = e[3], e[1]
-
-
-d = input()
-for i in d:
-    globals()[i.lower()]()
-
-print(e[1] * 1 + e[2] * 2 + e[3] * 3)
+print(sum([a * b for (a, b) in e.items()]))
