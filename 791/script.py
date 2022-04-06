c1327e9ade741ec310bb4109813228693ce979b7
Anton Erogov <anton2750@mail.ru>
2022-04-06 20:39:53 +0300

    submit

--- 791/script.py
+++ 791/script.py
@@ -1,22 +1,12 @@
 a = int(input())
-l, r, t, d = a - 1, a + 1, a - 8, a + 8
-
-res = []
-
-et = [i for i in range(1, 9)]
-el = [i * 8 + 1 for i in range(0, 8)]
-er = [i * 8 for i in range(1, 9)]
-ed = [56 + i for i in range(1, 9)]
-
-if a not in et:
-    res.append(t)
-if a not in el:
-    res.append(l)
-if a not in er:
-    res.append(r)
-if a not in ed:
-    res.append(d)
-
-res.sort()
-
-print(res)
+r = []
+if a not in [i for i in range(1, 9)]:
+    r.append(a - 8)
+if a not in [i * 8 + 1 for i in range(0, 8)]:
+    r.append(a - 1)
+if a not in [i * 8 for i in range(1, 9)]:
+    r.append(a + 1)
+if a not in [56 + i for i in range(1, 9)]:
+    r.append(a + 8)
+r.sort()
+print(*r)
