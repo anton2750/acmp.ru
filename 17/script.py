df32d43080e7718bd83cd15b769dbd4c85abdf7c
Anton Erogov <anton2750@mail.ru>
2022-04-08 14:21:50 +0300

    work

--- 17/script.py
+++ 17/script.py
@@ -1,26 +1,42 @@
 with open("input.txt", "r") as f:
     with open("output.txt", "w") as o:
-        a = int(f.readline())
-        numbers = list(map(int, f.readline().split()))
-        circle = [numbers[0]]
-        length = 1
+        total_length = int(f.readline())
+        input_numbers = list(map(int, f.readline().split()))
 
-        current = 0
-        temp = 0
-        for i in numbers:
-            current += 1
-            temp += 1
+        # setup:
+        circle = [input_numbers[0]]
+        circle_length = 1
+        circle_iterator = 0
+        global_iterator = 0
+        previous = 0
+
+        # loop:
+        for i in input_numbers:
+            global_iterator += 1
+            circle_iterator += 1
 
             try:
-                if i == circle[temp - 1]:
+                if i == circle[circle_iterator - 1]:
                     continue
             except IndexError:
                 pass
 
-            if i == circle[0]:
-                temp = 1
+            # suppose it is a new circle:
+            if i == circle[0] and circle_iterator - 1 == circle_length:
+                circle_iterator = 1
+            # suppose it is still the first loop:
             else:
-                circle = numbers[0:current]
-                length = current
+                circle = input_numbers[0:global_iterator]
+                circle_length = global_iterator
+                circle_iterator = global_iterator
 
-        o.write(str(len(circle)))
+                # hot fix:
+                if global_iterator == total_length:
+                    circle_length = total_length - 1
+
+        # hot fix:
+        # if previous != circle_length:
+        #     circle_length = total_length - 1
+
+        o.write(str(circle_length))
+        print(circle_length)
