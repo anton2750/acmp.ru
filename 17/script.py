8851bb4be69fd843d43dd87dc4f166653c3d334b
Anton Erogov <anton2750@mail.ru>
2022-04-08 23:36:49 +0300

    test number 6!!!

--- 17/script.py
+++ 17/script.py
@@ -1,3 +1,7 @@
+import time
+
+start_time = time.time()
+
 # case 1: one circle
 # case 2: several circles
 
@@ -13,34 +17,39 @@ with open("input.txt", "r") as f:
         global_iterator = 0
 
         # loop:
-        for i in input_numbers:
+        for i in input_numbers[0:total_length - 1]:
             global_iterator += 1
             circle_iterator += 1
 
             try:
+                # if number is in its place, everything's alright
                 if i == circle[circle_iterator - 1]:
                     continue
             except IndexError:
                 pass
 
-            # suppose it is a new circle:
-            # items must be equal to be first one
-            # circle iterator - 1 must be equal to circle length
             if i == circle[0] and circle_iterator - 1 == circle_length:
+                # suppose it is a new circle:
+                # iterator must be equal to the first element
+                # circle iterator number must be equal to the current circle length
                 circle_iterator = 1
-            # suppose it is still the first loop:
-            else:
-                circle = input_numbers[0:global_iterator]
-                circle_length = global_iterator
-                circle_iterator = global_iterator
+                continue
+                # suppose it is still the first loop:
 
-                # hot fix:
-                # if global_iterator == total_length:
-                #     circle_length = total_length - 1
+            # suppose we are still in the circle #1:
+            circle = input_numbers[0:global_iterator]
+            circle_length = global_iterator
+            circle_iterator = global_iterator
+
+            # hot fix:
+            # if global_iterator == total_length:
+            #     circle_length = total_length - 1
 
         # hot fix:
-        if circle_iterator != 1:
-            circle_length = total_length - 1
+        # if circle_iterator != 1:
+        #     circle_length = total_length - 1
 
         o.write(str(circle_length))
         print(circle_length)
+
+print("--- %s seconds ---" % "{:.2f}".format(time.time() - start_time))
